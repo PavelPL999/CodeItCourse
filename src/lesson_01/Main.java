3849a3c8c056cde3d2369fe1d21917a69823b527
Pavel <paleon@mail.ru>
2024-03-29 22:32:07 +0300

    Lesson 6

--- src/lesson_01/Main.java
+++ src/lesson_01/Main.java
@@ -2,13 +2,30 @@ package lesson_01;
 
 import lesson_03.Converter;
 import lesson_04.Calculator;
-
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Converter.convertMoney();
-        System.out.println("Add branch-one");
+        boolean isContinue = true;
+        int x = 0;
+        int y = 0;
+        while (isContinue) {
+            System.out.println("Сдвиньте робота: \n1 - left\n2- right\n3 - up\n4 - down");
+            int input = new Scanner(System.in).nextInt();
+            if (input == 1) {
+                x--;
+            } else if (input == 2) {
+                x++;
+            } else if (input == 3) {
+                y++;
+            } else if (input == 4) {
+                y--;
+            } else {
+                System.out.println("Error");
+                isContinue = false;
+            }
+            System.out.println("Координата X: " + x + " Координата Y: " + y);
+        }
     }
 }
